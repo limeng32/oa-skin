5f3a989fde22fbe02baa978c5ec6ee215af61453
limeng32 <limeng32@live.cn>
2015-04-24 19:07:54 +0800

    Develop the banner.

--- oa-skin/WebRoot/oa/build/article/article.js
+++ oa-skin/WebRoot/oa/build/article/article.js
@@ -1,158 +1,146 @@
-define(
-		'bee-demo/article/article',
-		[ "node", "./article-view", 'button', 'io', 'json', 'menu',
-				'separator', 'util', 'bee-demo/buttonPlus',
-				"kg/xtemplate/3.4.1/runtime" ],
-		function(require, exports, module) {
-			var $ = require('node').all;
-			var Node = require('node');
-			var Util = require('util');
-			var Menu = require('menu');
-			var Separator = require('separator');
-			var IO = require('io');
-			var JSON = require('json');
-			var BP = require('bee-demo/buttonPlus');
-			var XTemplate = require("kg/xtemplate/3.4.1/runtime");
-			module.exports = {
-				init : function() {
-					var BG = new Node('<div>').addClass('middleBanner');
-					var sorterOfTime = new Node('<div>').unselectable().append(
-							'到达时间').addClass('oa-sorter').addClass('ks-button')
-							.addClass('ks-button-mini').append(
-									new Node('<img>').prop({
-										src : 'image/arrowDownUnuse.png'
-									}).addClass('oa-sorter-arrow'));
-					var sorterOfUrgency = new Node('<div>').unselectable()
-							.append('缓急').addClass('oa-sorter').addClass(
-									'ks-button').addClass('ks-button-mini')
-							.append(new Node('<img>').prop({
-								src : 'image/arrowDownUnuse.png'
-							}).addClass('oa-sorter-arrow'));
-					var sorterOfCustom = new Node('<div>').unselectable()
-							.append('自定义').addClass('oa-sorter').addClass(
-									'ks-button').addClass('ks-button-mini')
-							.append(new Node('<img>').prop({
-								src : 'image/arrowDownUnuse.png'
-							}).addClass('oa-sorter-arrow'));
-					$('article').append(BG);
-					var sorterPanel = new Node('<div>').addClass('sorterPanel')
-							.addClass('ks-button-group');
-					BG.append(new Node('<div>').addClass('navigatorSpan').prop(
-							{
-								id : 'navigatorContainer'
-							}));
-					BG.append(
-							new Node('<div>').addClass('bannerSeparatorSpan')
-									.append(new Node('<img>').prop({
-										src : 'image/bannerSeparator.png'
-									}).addClass('bannerSeparator'))).append(
-							new Node('<div>').addClass('sorterPanelLeft')
-									.append('按')).append(sorterPanel).append(
+define('bee-demo/article/article', [ "node", "./article-view", 'button', 'io',
+		'json', 'menu', 'separator', 'util', 'bee-demo/buttonPlus',
+		"kg/xtemplate/3.4.1/runtime" ], function(require, exports, module) {
+	var $ = require('node').all;
+	var Node = require('node');
+	var Util = require('util');
+	var Menu = require('menu');
+	var Separator = require('separator');
+	var IO = require('io');
+	var JSON = require('json');
+	var BP = require('bee-demo/buttonPlus');
+	var XTemplate = require("kg/xtemplate/3.4.1/runtime");
+	module.exports = {
+		init : function() {
+			var BG = new Node('<div>').addClass('middleBanner');
+			var sorterOfTime = new Node('<div>').unselectable().append('到达时间')
+					.addClass('oa-sorter').addClass('ks-button').addClass(
+							'ks-button-mini').append(new Node('<img>').prop({
+						src : 'image/arrowDownUnuse.png'
+					}).addClass('oa-sorter-arrow'));
+			var sorterOfUrgency = new Node('<div>').unselectable().append('缓急')
+					.addClass('oa-sorter').addClass('ks-button').addClass(
+							'ks-button-mini').append(new Node('<img>').prop({
+						src : 'image/arrowDownUnuse.png'
+					}).addClass('oa-sorter-arrow'));
+			var sorterOfCustom = new Node('<div>').unselectable().append('自定义')
+					.addClass('oa-sorter').addClass('ks-button').addClass(
+							'ks-button-mini').append(new Node('<img>').prop({
+						src : 'image/arrowDownUnuse.png'
+					}).addClass('oa-sorter-arrow'));
+			$('article').append(BG);
+			var sorterPanel = new Node('<div>').addClass('sorterPanel')
+					.addClass('ks-button-group');
+			BG.append(new Node('<div>').addClass('navigatorSpan').prop({
+				id : 'navigatorContainer'
+			}));
+			BG.append(
+					new Node('<div>').addClass('bannerSeparatorSpan').append(
+							new Node('<img>').prop({
+								src : 'image/bannerSeparator.png'
+							}).addClass('bannerSeparator'))).append(
+					new Node('<div>').addClass('sorterPanelLeft').append('按'))
+					.append(sorterPanel).append(
 							new Node('<div>').addClass('sorterPanelRight')
 									.append('排序'));
-					sorterPanel.append(sorterOfTime).append(sorterOfUrgency)
-							.append(sorterOfCustom);
-					BP.tapelize(sorterPanel.children(), function(n) {
-						n.addClass('ks-button-pressed');
-						var src = n.one('.oa-sorter-arrow').prop('src');
-						if (Util.endsWith(src, 'Unuse.png')) {
-							n.one('.oa-sorter-arrow').prop('src',
-									src.substring(0, src.length - 9) + '.png');
-						} else {
-							if (Util.endsWith(src, 'Down.png')) {
-								n.one('.oa-sorter-arrow').prop(
-										'src',
-										src.substring(0, src.length - 8)
-												+ 'Up.png');
-							} else if (Util.endsWith(src, 'Up.png')) {
-								n.one('.oa-sorter-arrow').prop(
-										'src',
-										src.substring(0, src.length - 6)
-												+ 'Down.png');
-							}
-						}
-					}, function(n) {
-						n.removeClass('ks-button-pressed');
-						var src = n.one('.oa-sorter-arrow').prop('src');
-						if (!Util.endsWith(src, 'Unuse.png')) {
-							n.one('.oa-sorter-arrow').prop(
-									'src',
-									src.substring(0, src.length - 4)
-											+ 'Unuse.png');
-						}
-					});
-					BP.nOrders(sorterOfTime, [ function(n) {
-					}, function(n) {
-					} ]);
-
-					BP.nOrders(sorterOfUrgency, [ function(n) {
-					}, function(n) {
-					} ]);
-
-					BP.nOrders(sorterOfCustom, [ function(n) {
-					}, function(n) {
-					} ]);
-
-					IO
-							.get(
-									'../build/menuData.json',
-									{},
-									function(data) {
-										var sb = buildMenu(data, {
-											offset : [ 0, 36 ],
-											points : [ 'tl', 'tl' ]
-										});
-										var b = new Menu.SubMenu(
-												{
-													prefixCls : "nav-",
-													content : "<span class='title' style='width:58px'>首页</span>",
-													menu : sb
-												});
-										var menu = new Menu({
-											prefixCls : "nav-",
-											width : 90,
-											elCls : "horizonal",
-											render : '#navigatorContainer',
-											children : [ b ]
-										});
-
-										menu.render();
-
-										menu.on("click", function(e) {
-											console
-													.log(e.target
-															.get("content"));
-										});
-
-									}, 'json');
-					var buildMenu = function(data, align) {
-						var sb = new Menu.PopupMenu({
-							prefixCls : "nav-",
-							autoHideOnMouseLeave : true,
-							width : 150
-						});
-						if (align != null) {
-							sb.set('align', align);
-						}
-						for ( var o in data) {
-							var item;
-							if (data[o].sub != null) {
-								var sb2 = buildMenu(data[o].sub);
-								item = new Menu.SubMenu({
-									prefixCls : "nav-",
-									content : "<a>" + data[o].content + "</a>",
-									menu : sb2
-								});
-							} else {
-								item = new Menu.Item({
-									prefixCls : "nav-",
-									content : "<a>" + data[o].content + "</a>"
-								});
-							}
-							sb.addChild(item);
-						}
-						return sb;
+			sorterPanel.append(sorterOfTime).append(sorterOfUrgency).append(
+					sorterOfCustom);
+			BP.tapelize(sorterPanel.children(), function(n) {
+				n.addClass('ks-button-pressed');
+				var src = n.one('.oa-sorter-arrow').prop('src');
+				if (Util.endsWith(src, 'Unuse.png')) {
+					n.one('.oa-sorter-arrow').prop('src',
+							src.substring(0, src.length - 9) + '.png');
+				} else {
+					if (Util.endsWith(src, 'Down.png')) {
+						n.one('.oa-sorter-arrow').prop('src',
+								src.substring(0, src.length - 8) + 'Up.png');
+					} else if (Util.endsWith(src, 'Up.png')) {
+						n.one('.oa-sorter-arrow').prop('src',
+								src.substring(0, src.length - 6) + 'Down.png');
 					}
 				}
+			}, function(n) {
+				n.removeClass('ks-button-pressed');
+				var src = n.one('.oa-sorter-arrow').prop('src');
+				if (!Util.endsWith(src, 'Unuse.png')) {
+					n.one('.oa-sorter-arrow').prop('src',
+							src.substring(0, src.length - 4) + 'Unuse.png');
+				}
+			});
+			BP.nOrders(sorterOfTime, [ function(n) {
+			}, function(n) {
+			} ]);
+
+			BP.nOrders(sorterOfUrgency, [ function(n) {
+			}, function(n) {
+			} ]);
+
+			BP.nOrders(sorterOfCustom, [ function(n) {
+			}, function(n) {
+			} ]);
+
+			IO.get('menuData.json', {}, function(data) {
+				var sb = buildMenu(data, {
+					offset : [ 5, 36 ],
+					points : [ 'tl', 'tl' ]
+				});
+				var b = new Menu.SubMenu({
+					prefixCls : "nav-",
+					content : "<span class='title'>首页</span>",
+					menu : sb
+				});
+				var menu = new Menu({
+					prefixCls : "nav-",
+					width : 90,
+					elCls : "horizonal",
+					render : '#navigatorContainer',
+					children : [ b ]
+				});
+
+				menu.render();
+
+				menu.on("click", function(e) {
+					console.log(e.target.get("content"));
+					var content = e.target.get("content");
+					if (Util.startsWith(content, '<a>')
+							&& Util.endsWith(content, '</a>')) {
+						content = content.substring(3, content.length);
+						content = content.substring(0, content.length - 4);
+						b.set('content', "<span class='title'>" + content
+								+ "</span>");
+					}
+				});
+
+			}, 'json');
+			var buildMenu = function(data, align) {
+				var sb = new Menu.PopupMenu({
+					prefixCls : "nav-",
+					autoHideOnMouseLeave : true,
+					width : 150
+				});
+				if (align != null) {
+					sb.set('align', align);
+				}
+				for ( var o in data) {
+					var item;
+					if (data[o].sub != null) {
+						var sb2 = buildMenu(data[o].sub);
+						item = new Menu.SubMenu({
+							prefixCls : "nav-",
+							content : "<a>" + data[o].content + "</a>",
+							menu : sb2
+						});
+					} else {
+						item = new Menu.RadioItem({
+							prefixCls : "nav-",
+							content : "<a>" + data[o].content + "</a>"
+						});
+					}
+					sb.addChild(item);
+				}
+				return sb;
 			}
-		});
+		}
+	}
+});
--- oa-skin/WebRoot/oa/build/header/header.js
+++ oa-skin/WebRoot/oa/build/header/header.js
@@ -1,53 +1,42 @@
-define('bee-demo/header/header', [ "node", "io" ], function(require, exports,
-		module) {
-	var $ = require('node').all;
-	var Node = require('node');
-	var IO = require('io');
-	module.exports = {
-		init : function() {
-			var headerBG = new Node('<div>').addClass('banner');
-			var logoKnot = new Node('<img>').prop({
-				src : 'image/logoKnot.png'
-			}).addClass('logoKnot');
-			var logoText = new Node('<img>').prop({
-				src : 'image/logoText.png'
-			}).addClass('logoText');
-			$('header').append(headerBG.append(logoKnot).append(logoText));
-			var rightBox = new Node('<div>').addClass('rightBox');
-			rightBox.append(new Node('<div>').addClass('headerSetting').append(
-					new Node('<img>').prop({
-						src : 'image/headerSetting.png'
-					})));
-			rightBox.append(new Node('<div>').addClass('headerHelping').append(
-					new Node('<img>').prop({
-						src : 'image/headerHelping.png'
-					})));
-			headerBG.append(rightBox);
-			var welcomePanel = new Node('<div>').addClass('welcomePanel')
-					.append(new Node('<img>').prop({
-						src : 'image/welcomePanel.png'
-					}));
-			var welcomePerson = new Node('<div>').addClass('welcomePerson');
-			headerBG.append(welcomePanel.append(welcomePerson));
-			initWelcome(welcomePerson);
-			var rightPanel = new Node('<div>').addClass('rightPanel');
-			var favoritesPanel = new Node('<div>').addClass('rightPanelItems')
-					.append(new Node('<img>').prop({
-						src : 'image/favoritesPanel.png'
-					}));
-			var filesPanel = new Node('<div>').addClass('rightPanelItems')
-					.append(new Node('<img>').prop({
-						src : 'image/filesPanel.png'
-					}));
-			headerBG.append(rightPanel.append(filesPanel)
-					.append(favoritesPanel));
-		}
-	}
-	function initWelcome(welcomePerson) {
-		IO.get('loginPerson.json', {
-			'index' : 1
-		}, function(data) {
-			welcomePerson.html(data.loginPerson.name);
-		}, 'json');
-	}
-});
+define(
+		'bee-demo/header/header',
+		[ "node", "io" ],
+		function(require, exports, module) {
+			var $ = require('node').all;
+			var Node = require('node');
+			var IO = require('io');
+			module.exports = {
+				init : function() {
+					var headerBG = new Node('<div>').addClass('banner');
+					var logoKnot = new Node('<div>').addClass('image logoKnot');
+					var logoText = new Node('<div>').addClass('image logoText');
+					$('header').append(
+							headerBG.append(logoKnot).append(logoText));
+					var welcomePanel = new Node('<div>')
+							.addClass('image welcomePanel');
+					var welcomeImage = new Node('<div>')
+							.addClass('image welcomeImage');
+					var welcomePerson = new Node('<div>')
+							.addClass('welcomePerson');
+					headerBG.append(welcomePanel.append(welcomeImage).append(
+							welcomePerson));
+					initWelcome(welcomePerson);
+					var rightPanel = new Node('<div>').addClass('rightPanel');
+					var headerSetting = new Node('<div>')
+							.addClass('rightPanelItems image headerSetting');
+					var headerSearch = new Node('<div>')
+							.addClass('rightPanelItems image headerSearch');
+					var headerHelping = new Node('<div>')
+							.addClass('rightPanelItems image headerHelping');
+					headerBG.append(rightPanel.append(headerSetting).append(
+							headerSearch).append(headerHelping));
+				}
+			}
+			function initWelcome(welcomePerson) {
+				IO.get('loginPerson.json', {
+					'index' : 1
+				}, function(data) {
+					welcomePerson.html(data.loginPerson.name);
+				}, 'json');
+			}
+		});
